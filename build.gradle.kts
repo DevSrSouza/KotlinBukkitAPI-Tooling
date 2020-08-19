82af2f841833ab86dc0ae3f5fc1f812124ffaf9e
DevSrSouza <devsrsouza@gmail.com>
2020-08-19 09:29:09 -0300

    version 0.0.1

--- build.gradle.kts
+++ build.gradle.kts
@@ -9,7 +9,7 @@ val kotlinBukkitAPIVersion = "0.1.0-SNAPSHOT"
 val bukkriptVersion = "0.1.0-SNAPSHOT"
 
 group = "br.com.devsrsouza.kotlinbukkitapi"
-version = "0.0.1-SNAPSHOT"
+version = "0.0.1"
 
 repositories {
     jcenter()
